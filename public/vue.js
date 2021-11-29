b1efe0ae5b7483a2809517b67ce802d42b8f90a3
Andrew Glen <andrew@glenagon.com>
2021-11-29 18:03:54 +0000

    Add basic 'new game' selection page

--- public/vue.js
+++ public/vue.js
@@ -3,13 +3,22 @@ var socket = null
 var app = new Vue({
   el: '#vue-app',
   data: {
-
+    decks: [
+      "Computer Science History",
+      "Movies 1960-1990",
+      "Pop Songs 1980-2010",
+      "19th Century Geopolitical Conflicts",
+      "20th Century Scientists",
+    ],
+    selectedDeckIndex: null,
   },
   mounted: function () {
     connect()
   },
   methods: {
-
+    selectDeckAt: function(index) {
+      this.selectedDeckIndex = index
+    },
   }
 })
 

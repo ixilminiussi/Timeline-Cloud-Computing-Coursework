0f4291525880786483fb5e8f2a0065b3e94716b7
Andrew Glen <andrew@glenagon.com>
2021-11-29 18:03:37 +0000

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: colors.cyan,
+        gray: colors.blueGray,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

ada9ed27e8746958fb597a760ed0f55e108ef9b0
ixil <idm1u19@soton.ac.uk>
2021-12-28 12:09:13 +0100

    changed structure, added close button

---
+++ public/account.js
@@ -0,0 +1,62 @@
+var user = new Vue({
+    el: '#account',
+    data: {
+        me: { status: -1, username: '', password: '', email: '' }, // -1 - disconnected; 1 - connected
+        form: { show: -1, passwordInput: 'password' }, // -1 - nothing; 0 - show login form; 1 - show register form; 2 - show account applet (profile, disconnect)
+    },
+    mounted: function() {
+        connect();
+
+        // Allows for closing the login form with keypress
+        window.addEventListener('keydown', (e) => {
+            if (e.key == 'Escape') {
+                this.closeLoginForm();
+            }
+        });
+
+        // Allows for closing the login form by clicking outside
+        window.addEventListener('mousedown', (e) => {
+            if (this.form.show != -1 && !document.getElementById('loginForm').contains(e.target)) {
+                this.closeLoginForm();
+            }
+        });
+    },
+    methods: {
+        login: function(username, password) {
+
+        },
+        signup: function(username, password, email) {
+
+        },
+        createDeck: function() {
+
+        },
+        importDecks: function() {
+
+        },
+        showLoginForm: function() {
+            this.form.show = 0;
+        },
+        showSignupForm: function() {
+            this.form.show = 1;
+        },
+        closeLoginForm: function() {
+            this.form.show = -1;
+        },
+        openAccount: function() {
+            if (this.me.status == -1) {
+                this.status = 0;
+            }
+        },
+        closeAccount: function() {
+            this.me.status = -1;
+        },
+        togglePassword: function() {
+            if (this.form.passwordInput == 'password') {
+                this.form.passwordInput = 'text';
+            } else {
+                this.form.passwordInput = 'password';
+            }
+        }
+    }
+});

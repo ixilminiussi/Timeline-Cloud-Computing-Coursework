c1d979a9b3fd3ca3bf0e93c1e1c1f6a3cfb8b4d5
arg1g19 <arg1g19@soton.ac.uk>
2021-12-29 14:14:17 +0000

    Document the utility functions

--- utility.js
+++ utility.js
@@ -1,5 +1,16 @@
 // Miscellaneous helper functions
 
+/**
+ * A convenience function to asynchronously delaying execution.
+ * @example
+ * async function foo() {
+ *   bar()
+ *   await chill(1000) // Wait for 1 second
+ *   baz()
+ * }
+ * @param {number} duration The number of milliseconds to wait for.
+ * @returns {Promise} a promise resolving in `duration` milliseconds.
+ */
 const chill = (duration) => new Promise(resolve => setTimeout(resolve, duration))
 
 module.exports = {

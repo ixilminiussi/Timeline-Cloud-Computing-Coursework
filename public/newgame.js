9e08a14961a809a7f6109f14c3cea6794bf3b9ec
arg1g19 <arg1g19@soton.ac.uk>
2022-01-03 20:09:42 +0000

    Send available decks to clients

--- public/newgame.js
+++ public/newgame.js
@@ -3,13 +3,7 @@ var socket = null
 var app = new Vue({
   el: '#vue-app',
   data: {
-    decks: [
-      "Computer Science History",
-      "Movies 1960-1990",
-      "Pop Songs 1980-2010",
-      "19th Century Geopolitical Conflicts",
-      "20th Century Scientists",
-    ],
+    decks: [],
     selectedDeckIndex: null,
     joinLink: "Loading...",
     copiedJoinLink: false,

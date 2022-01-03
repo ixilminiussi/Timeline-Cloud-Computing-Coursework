a664dedc0d0e7748451900384da287452e5ac23f
arg1g19 <arg1g19@soton.ac.uk>
2022-01-03 23:11:05 +0000

    Add pattern to background of hand

--- public/game.js
+++ public/game.js
@@ -249,6 +249,10 @@ var app = new Vue({
   computed: {
     isMyTurn: function () {
       return this.currentTurn === this.username && this.username !== ""
+    },
+    background: function() {
+      return `background-color: #f8fafc;
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23f1f5f9' fill-opacity='1'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");`
     }
   }
 })
